471674b50ec3c94b2e8856812a4db7eec0389af5
Happitack <70133275+Happitack@users.noreply.github.com>
2023-06-20 15:07:34 +0200

    Added a few comments about server flow

--- server.js
+++ server.js
@@ -2,16 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+// Creates an Express application
 const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
 
+// The port that our server will run on (localhost:3001 by default)
 const port = process.env.PORT || 3001;
+// Starting the server on our specified port
 app.listen(port, () => console.log(`Server is running on ${port}`));
 
+// Importing the mssql package to interact with an MS SQL database
 const sql = require('mssql');
 
+// Configuration for connecting to our database
 const config = {
   user: 'sa',
   password: 'password',
@@ -22,10 +27,12 @@ const config = {
   }
 };
 
+// Connecting to the database
 sql.connect(config)
 .then(() => console.log('Database connected'))
 .catch(err => console.log('Database connection failed', err));
 
+// Defining a route for a POST request to /subscribe. This is where the client will send the email to be subscribed.
 app.post('/subscribe', (req, res) => {
   const email = req.body.email;
 
@@ -41,6 +48,7 @@ app.post('/subscribe', (req, res) => {
   });
 });
 
+// Defines a route for a GET request to /textdata. This is where the client will fetch the text data for the header.
 app.get('/textdata', (req, res) => {
   const request = new sql.Request();
   request.query("SELECT Title, Subtitle, Description FROM TextData WHERE Page = 'header'", (err, result) => {

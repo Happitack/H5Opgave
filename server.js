4707054704e095247e4b71235b479300c23c4cf1
Happitack <70133275+Happitack@users.noreply.github.com>
2023-06-22 09:47:30 +0200

    Updated server.js script to auto create database

--- server.js
+++ server.js
@@ -17,20 +17,69 @@ app.listen(port, () => console.log(`Server is running on ${port}`));
 const sql = require('mssql');
 
 // Configuration for connecting to our database
+// This is not secure and should be done differently in production
 const config = {
   user: 'sa',
   password: 'password',
   server: 'localhost',
   database: 'TwinMindsDB',
   options: {
-    encrypt: false 
+    encrypt: false,
+    enableArithAbort: true 
   }
 };
 
-// Connecting to the database
-sql.connect(config)
-.then(() => console.log('Database connected'))
-.catch(err => console.log('Database connection failed', err));
+// Here we define the SQL commands that we want to run on server start
+// This is not secure and should be done differently in production
+const sqlCommands = [
+  `IF NOT EXISTS (
+      SELECT name FROM sys.databases WHERE name = 'TwinMindsDB'
+  )
+  CREATE DATABASE TwinMindsDB`,
+  "USE TwinMindsDB",
+  `IF NOT EXISTS (
+      SELECT * FROM sys.tables WHERE name='Subscribers' AND type = 'U'
+  )
+  CREATE TABLE Subscribers(
+      ID INT PRIMARY KEY IDENTITY(1,1),
+      Email NVARCHAR(320) NOT NULL,
+      SubscriptionDate DATETIME DEFAULT GETDATE()
+  )`,
+  `IF NOT EXISTS (
+      SELECT * FROM sys.tables WHERE name='TextData' AND type = 'U'
+  )
+  CREATE TABLE TextData (
+      ID INT IDENTITY(1,1) PRIMARY KEY,
+      Page NVARCHAR(100),
+      Title NVARCHAR(200),
+      Subtitle NVARCHAR(200),
+      Description NVARCHAR(MAX)
+  )`,
+  `IF NOT EXISTS (
+      SELECT * FROM TextData WHERE Page = 'paranoia'
+  )
+  INSERT INTO TextData (Page, Title, Subtitle, Description) VALUES ('paranoia', 'Paranoia', 'The Concepts of Loneliness', 'Description...')`,
+  `IF NOT EXISTS (
+      SELECT * FROM TextData WHERE Page = 'projectfear'
+  )
+  INSERT INTO TextData (Page, Title, Subtitle, Description) VALUES ('projectfear', 'Project Fear', 'The Concepts of Loneliness', 'Description...')`
+];
+
+// Connect to SQL Server and run commands
+sql.connect(config).then(pool => {
+  // Execute each SQL command sequentially
+  return sqlCommands.reduce((prev, command) => {
+      return prev.then(() => {
+          return pool.request().query(command);
+      });
+  }, Promise.resolve()); // Initial promise to start the chain
+}).then(() => {
+  console.log('Database setup successful');
+}).catch(err => {
+  console.error('Database setup failed', err);
+}).finally(() => {
+  sql.close(); // close the connection pool
+});
 
 // Defining a route for a POST request to /subscribe. This is where the client will send the email to be subscribed.
 app.post('/subscribe', (req, res) => {
